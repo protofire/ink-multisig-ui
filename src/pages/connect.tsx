8282993725884b389643014ab38be1bb3d5d2958
Henry Palacios <henrydpalacios@gmail.com>
2023-07-20 12:32:33 -0300

    Adding useRedirectComparison

--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -1,21 +1,29 @@
 import { Button, CardMedia, Typography } from "@mui/material";
 import Image from "next/image";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 import { BasicLayout } from "@/components/layout/BasicLayout";
 import { MainContentCard } from "@/components/layout/shared/MainContentCard";
 import { CRYPTO_WALLET } from "@/config/images";
 import { ROUTES } from "@/config/routes";
+import useRedirectComparison from "@/hooks/useRedirectComparison";
 
 export function getTextInstructions(route: string) {
   if (route === ROUTES.New) {
     return "To create a new account";
   } else if (route === ROUTES.Load) {
-    return "To create a new account";
+    return "To load an existing account";
   }
 }
 
 export default function ConnectWalletPage() {
+  const [instructions, setInstructions] = useState<string | undefined>();
+
+  useRedirectComparison({
+    redirectUrls: [ROUTES.New, ROUTES.Load],
+    onMatch: (route) => setInstructions(getTextInstructions(route)),
+  });
+
   return (
     <MainContentCard
       title="Connect your Wallet"
@@ -30,12 +38,11 @@ export default function ConnectWalletPage() {
           height={256}
         />
       </CardMedia>
-      {/* TODO 
       {instructions && (
-        <Typography variant="h3">
+        <Typography variant="body1">
           {instructions}, you will need a wallet to sign the transaction
         </Typography>
-      )} */}
+      )}
       <Button size="large">
         <Typography variant="h3">Connect your wallet</Typography>
       </Button>
