5775a93a5b14d5c2c902d1202624c744b8e1735b
Henry Palacios <henrydpalacios@gmail.com>
2023-08-07 18:23:31 -0300

    Adding Dark and light mode on storybook (#15)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,8 @@
-import { StoryFn, type Preview } from "@storybook/react";
+import { StoryFn, type Preview, StoryContext } from "@storybook/react";
 import ThemeCustomization from "@/themes";
-import React from "react";
+import { Settings } from "@/themes/types";
+
+import React, {useEffect, useMemo, useState} from "react";
 
 
 const preview: Preview = {
@@ -13,20 +15,40 @@ const preview: Preview = {
       },
     },
   },
+  globalTypes: {
+    theme: {
+      name: "Theme",
+      title: "Theme",
+      description: "Theme for your components",
+      defaultValue: "light",
+      toolbar: {
+        icon: "paintbrush",
+        dynamicTitle: true,
+        items: [
+          { value: "light", left: "☀️", title: "Light mode" },
+          { value: "dark", left: "🌙", title: "Dark mode" },
+        ],
+      },
+    },
+  }
 };
 
 export default preview;
 
-export const withMuiTheme = (Story: StoryFn) => {
-  const settings = {
-    navOpen: true,
-    mode: "dark",
-    skin: "default",
-    drawerWidth: 240,
-  };
+const defaultSettings: Settings = {
+  navOpen: true,
+  mode: "dark",
+  skin: "default",
+  drawerWidth: 240,
+};
+
+
+export const withMuiTheme = (Story: StoryFn, context: StoryContext) => {
+  const { theme: themeKey } = context.globals;
+  const _settings = useMemo(() => ({...defaultSettings, mode: themeKey}), [themeKey])
   
   return (
-    <ThemeCustomization settings={settings}>
+    <ThemeCustomization settings={_settings}>
       <Story />
     </ThemeCustomization>
   )
