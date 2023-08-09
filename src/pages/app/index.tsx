2d053bcae34e24de8951737ecb703dc1cfffa504
Henry Palacios <henrydpalacios@gmail.com>
2023-08-09 18:25:10 -0300

    theme mode colors (#17)
    * Adding Colors
    
    * Switching colors in all theme
    
    * Improving theme palette mode colors using rgb

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,3 +1,14 @@
+import { Card, Grid } from "@mui/material";
+
 export default function AppDashboard() {
-  return <>App Dashboard</>;
+  return (
+    <Grid container>
+      <Grid item xs={12} component="section">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </Grid>
+      <Grid item>Widgets</Grid>
+    </Grid>
+  );
 }
