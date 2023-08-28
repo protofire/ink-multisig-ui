bbaf89698c29901881217119652f84923695e253
Henry Palacios <henrydpalacios@gmail.com>
2023-08-28 08:50:33 -0300

    Removing unnecessary packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,11 +8,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    'storybook-addon-module-mock',
   ],
-  features: {
-    storyStoreV7: true,
-  },
   framework: {
     name: "@storybook/nextjs",
     options: {},
