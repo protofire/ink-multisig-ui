b5fc0d1892c9d9723988c42349074b7703dc7226
Agustín Longoni <agustin.longoni@altoros.com>
2023-08-29 10:55:37 -0300

    Home UI fixes + balance popover (#34)
    * add balance popover
    
    * add total balance
    
    * refactor
    
    * fix error
    
    * fix storybook and refactor
    
    * styles
    
    * Fix apiPromise
    
    ---------
    
    Co-authored-by: Henry Palacios <henrydpalacios@gmail.com>

--- src/components/SummaryCard/XsignerBalanceText.tsx
+++ src/components/SummaryCard/XsignerBalanceText.tsx
@@ -1,18 +1,92 @@
-import { Box, BoxProps, Typography } from "@mui/material";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import { Box, BoxProps, Divider, Popover, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import React from "react";
 
-const BoxWrapper = styled(Box)<BoxProps>(() => ({}));
+import { XsignerBalance } from "@/hooks/useGetBalance";
+
+const BoxWrapper = styled(Box)<BoxProps>(() => ({
+  padding: "1rem",
+  minWidth: "18rem",
+}));
 
 interface Props {
-  freeBalance: string | undefined;
-  reservedBalance: string | undefined;
+  balance: XsignerBalance;
 }
 
-export function XsignerBalanceText({ freeBalance, reservedBalance }: Props) {
+export function XsignerBalanceText({ balance }: Props) {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+  const { freeBalance, reservedBalance, totalBalance } = balance;
+  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
   return (
-    <BoxWrapper>
-      <Typography>{freeBalance}</Typography>
-      <Typography>{reservedBalance}</Typography>
-    </BoxWrapper>
+    <>
+      <Typography
+        component="div"
+        aria-owns={open ? "mouse-over-popover" : undefined}
+        aria-haspopup="true"
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}
+        sx={{ display: "flex", gap: "0.4rem" }}
+      >
+        <Typography variant="h3" color="white" component="span">
+          {freeBalance}
+        </Typography>{" "}
+        <InfoOutlinedIcon fontSize="medium" />
+      </Typography>
+      <Popover
+        id="mouse-over-popover"
+        sx={{
+          pointerEvents: "none",
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "center",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "center",
+          horizontal: "left",
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <BoxWrapper>
+          <Box display={"flex"} justifyContent={"space-between"}>
+            <Typography variant="body1" component="span">
+              Transferrable:
+            </Typography>{" "}
+            <Typography variant="body1" component="span">
+              {freeBalance}
+            </Typography>
+          </Box>
+          <Box display={"flex"} justifyContent={"space-between"}>
+            <Typography variant="body1" component="span">
+              Reserved:
+            </Typography>
+            <Typography variant="body1" component="span">
+              {reservedBalance}
+            </Typography>
+          </Box>
+          <Divider />
+          <Box display={"flex"} justifyContent={"space-between"} mt={1}>
+            <Typography variant="body1" fontWeight={800} component="span">
+              Total Balance:{" "}
+            </Typography>
+            <Typography variant="body1" fontWeight={800} component="span">
+              {totalBalance}
+            </Typography>
+          </Box>
+        </BoxWrapper>
+      </Popover>
+    </>
   );
 }
