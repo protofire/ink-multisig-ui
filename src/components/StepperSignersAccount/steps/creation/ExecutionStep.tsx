3aac80eba4d97d878ba86d8cc264799cbaecabbf
Agustín Longoni <agustin.longoni@altoros.com>
2023-09-11 18:45:49 -0300

    add circular progress component

--- src/components/StepperSignersAccount/steps/creation/ExecutionStep.tsx
+++ src/components/StepperSignersAccount/steps/creation/ExecutionStep.tsx
@@ -1,11 +1,12 @@
-import { Box, Typography } from "@mui/material";
+import { Box, CircularProgress, Typography } from "@mui/material";
 
 import { StyledBox } from "../../styled";
 
 function ExecutionStep() {
   return (
     <Box>
-      <StyledBox mt={2} mb={1} gap={4}>
+      <StyledBox mt={4} mb={4} gap={4} alignItems="center">
+        <CircularProgress />
         <Typography variant="caption">
           Please wait a few seconds while this is processed
         </Typography>
