b7b126f359df7c126b7ea321f1642597f154055c
Henry Palacios <henrydpalacios@gmail.com>
2023-09-11 17:30:09 -0300

    Adding NPrgress bar (#50)

---
+++ src/themes/GlobalStyling.ts
@@ -0,0 +1,20 @@
+import { Theme } from "@mui/material/styles";
+
+const GlobalStyles = (theme: Theme) => {
+  return {
+    "#nprogress": {
+      pointerEvents: "none",
+      "& .bar": {
+        left: 0,
+        top: 0,
+        height: 3,
+        width: "100%",
+        zIndex: 2000,
+        position: "fixed",
+        backgroundColor: theme.palette.primary.main,
+      },
+    },
+  };
+};
+
+export default GlobalStyles;
