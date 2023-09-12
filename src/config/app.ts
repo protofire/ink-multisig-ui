38bc894cd5d49091be1f47c504228d59c2ffc3da
Henry Palacios <henrydpalacios@gmail.com>
2023-09-12 20:05:35 -0300

    Use VERCEL_ENV on environment constant (#53)
    * Use VERCEL_ENV on environment const
    
    * Using isDevelopment
    
    * Using NEXT_PUBLIC prefix

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,2 +1,4 @@
 export const IS_DEVELOPMENT =
-  process.env.NODE_ENV === ("development" as string);
+  process.env.NODE_ENV === ("development" as string) ||
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ||
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
