bba6e9b19a0ceae81008cf94808001b7183167d3
Henry Palacios <henrydpalacios@gmail.com>
2023-09-12 19:44:27 -0300

    Using isDevelopment

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,3 +1,4 @@
 export const IS_DEVELOPMENT =
   process.env.NODE_ENV === ("development" as string) ||
-  process.env.VERCEL_ENV === "preview";
+  process.env.VERCEL_ENV === "preview" ||
+  process.env.VERCEL_ENV === "development";
