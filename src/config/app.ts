ad9dbe1b3187edd6cc9f32fce8d3dc1fab11a7b7
Henry Palacios <henrydpalacios@gmail.com>
2023-09-12 19:57:25 -0300

    Using NEXT_PUBLIC prefix

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,4 +1,4 @@
 export const IS_DEVELOPMENT =
   process.env.NODE_ENV === ("development" as string) ||
-  process.env.VERCEL_ENV === "preview" ||
-  process.env.VERCEL_ENV === "development";
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ||
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
