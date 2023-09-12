96b11d4d9ae4d60fdc43db851735d9e3d5b4c5ba
Henry Palacios <henrydpalacios@gmail.com>
2023-09-12 18:52:08 -0300

    Use VERCEL_ENV on environment const

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,2 +1,3 @@
 export const IS_DEVELOPMENT =
-  process.env.NODE_ENV === ("development" as string);
+  process.env.NODE_ENV === ("development" as string) ||
+  process.env.VERCEL_ENV === "preview";
