95bb3161684811973a63ddea7da649d7b6817721
Mati [C] Dastugue <mdastugu@amazon.com>
2023-11-09 18:37:19 -0300

    Update execution view

--- src/components/StepperSignersAccount/steps/creation/ExecutionStep.tsx
+++ src/components/StepperSignersAccount/steps/creation/ExecutionStep.tsx
@@ -2,6 +2,7 @@ import { Box, CircularProgress, Typography } from "@mui/material";
 
 import { ValidationError } from "@/hooks/xsignersAccount/useFormSignersAccountState";
 
+import { CREATION_STEPS } from "../../constants";
 import { StyledBox } from "../../styled";
 
 function ExecutionStep({
@@ -12,15 +13,22 @@ function ExecutionStep({
   step: number;
 }) {
   const hasErrors = errors[step].some((error) => error.error);
+  const stepCompleted = CREATION_STEPS.execution.length - 1 === step;
+
+  const renderMessage = () => {
+    if (hasErrors) {
+      return "There was an error while processing the transaction, please try again.";
+    }
+    if (!stepCompleted) {
+      return "Please wait a few seconds while this is processed";
+    }
+    return "Your XSigners Account was successfully created!";
+  };
   return (
     <Box>
       <StyledBox mt={4} mb={4} gap={4} alignItems="center">
-        {!hasErrors && <CircularProgress />}
-        <Typography variant="caption">
-          {!hasErrors
-            ? "Please wait a few seconds while this is processed"
-            : "There was an error while processing the transaction, please try again."}
-        </Typography>
+        {!hasErrors && !stepCompleted && <CircularProgress />}
+        <Typography variant="caption">{renderMessage()}</Typography>
       </StyledBox>
     </Box>
   );
