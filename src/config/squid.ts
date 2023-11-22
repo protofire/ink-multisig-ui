83991fe47f9b542303e9055f52009b188b0f4ae6
Henry Palacios <henrydpalacios@gmail.com>
2023-11-22 15:32:19 -0300

    Testing require

--- src/config/squid.ts
+++ src/config/squid.ts
@@ -1,4 +1,7 @@
-type ChainId = import("@/services/useink/types").ChainId;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const types = require("@/services/useink/types");
+
+type ChainId = typeof types.ChainId;
 
 export const squidConfig: Partial<Record<ChainId, string>> = {
   "shibuya-testnet": "http://18.118.77.170:4350/graphql",
