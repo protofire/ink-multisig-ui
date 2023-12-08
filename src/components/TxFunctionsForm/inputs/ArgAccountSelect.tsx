854e7596b73bae9d771bbe5b28784908b3dbb44d
Henry Palacios <henrydpalacios@gmail.com>
2023-12-08 09:29:57 -0300

    Fix AddressAccount Select

--- src/components/TxFunctionsForm/inputs/ArgAccountSelect.tsx
+++ src/components/TxFunctionsForm/inputs/ArgAccountSelect.tsx
@@ -1,8 +1,23 @@
+import { useMemo } from "react";
+
 import { AddressAccountSelect } from "@/components/AddressAccountSelect";
+import { usePolkadotContext } from "@/context/usePolkadotContext";
 import { ArgumentComponentProps } from "@/domain/substrateInputTypes";
 
 type ArgAccountSelectProps = ArgumentComponentProps<string>;
 
 export function ArgAccountSelect({ ...props }: ArgAccountSelectProps) {
-  return <AddressAccountSelect {...props} />;
+  const { accounts } = usePolkadotContext();
+  const options = useMemo(
+    () =>
+      accounts
+        ? accounts.map((account) => ({
+            address: account.address,
+            type: "Accounts",
+          }))
+        : [],
+    [accounts]
+  );
+
+  return <AddressAccountSelect options={options} {...props} />;
 }
