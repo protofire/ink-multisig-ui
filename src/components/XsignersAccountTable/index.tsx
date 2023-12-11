0c7a4d3ababd0fd0f8deff1841653ced4317e6ed
Agustín Longoni <agustin.longoni@altoros.com>
2023-12-11 11:22:52 -0300

    add XsignerAccountTable component

---
+++ src/components/XsignersAccountTable/index.tsx
@@ -0,0 +1,145 @@
+import { HowToReg } from "@mui/icons-material";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+import { Box, Tooltip, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import router from "next/router";
+import { ChainId } from "useink/dist/chains";
+
+import { MainContentCard } from "@/components/layout/shared/MainContentCard";
+import NetworkBadge from "@/components/NetworkBadge";
+import { AccountSigner } from "@/components/StepperSignersAccount/AccountSigner";
+import { ChainExtended, getChain } from "@/config/chain";
+import { ROUTES } from "@/config/routes";
+import { SignatoriesAccount } from "@/domain/SignatoriesAccount";
+
+export type MultisigsDataFormatted = {
+  name: string;
+  address: string;
+  networkId?: ChainId;
+  network?: ChainExtended;
+};
+
+interface Props {
+  multisigs: Array<MultisigsDataFormatted>;
+  onClick: (account: SignatoriesAccount) => Promise<void | SignatoriesAccount>;
+  network: ChainId;
+}
+
+export function XsignersAccountTable({
+  multisigs,
+  onClick: setXsigner,
+  network,
+}: Props) {
+  const { logo, name: networkName } = getChain(network);
+  const theme = useTheme();
+
+  const handleMultisigRedirect = (address: string) => {
+    const selectedMultisig = multisigs?.find(
+      (multisig) => multisig.address === address
+    );
+    if (!selectedMultisig) {
+      return;
+    }
+    setXsigner(selectedMultisig as SignatoriesAccount);
+    router.replace(ROUTES.App);
+  };
+
+  return (
+    <MainContentCard
+      stylesContainer={{
+        alignItems: "center",
+        sx: {
+          margin: { xs: 1, sm: 2, md: 2, lg: 3, xl: 3 },
+        },
+      }}
+    >
+      <Box display="flex" alignItems="center" gap={1.25}>
+        <Typography variant="h3" color="white">
+          My XSigners accounts ({multisigs.length})
+        </Typography>
+        <Typography variant="body1" component="p">
+          on
+        </Typography>
+        <NetworkBadge
+          showTooltip={false}
+          name={networkName}
+          logo={logo.src}
+          logoSize={{ width: 20, height: 20 }}
+          description={logo.alt}
+        />
+      </Box>
+      <Box
+        mt={3}
+        width="100%"
+        sx={{ backgroundColor: theme.palette.background.paper }}
+        p={0}
+        pt={0}
+        pb={0}
+      >
+        {multisigs.map((multisig) => (
+          <Box key={multisig.address}>
+            <Box
+              onClick={() => handleMultisigRedirect(multisig.address)}
+              display="flex"
+              gap={8}
+              alignItems="center"
+              justifyContent="space-around"
+              sx={{
+                borderBottom: "1px solid #2F2F2F",
+                borderTop: "1px solid #2F2F2F",
+                "&:hover": { backgroundColor: "#2F2F2F" },
+              }}
+              p={1}
+              pl={0}
+            >
+              <Box width={300}>
+                <AccountSigner
+                  name={multisig.name}
+                  address={multisig.address}
+                  truncateAmount={16}
+                  showLink={false}
+                />
+              </Box>
+              <Box display="flex" alignItems="center" gap={1}>
+                <HowToReg
+                  sx={{
+                    fontSize: "1.4rem",
+                    color: theme.palette.primary.main,
+                  }}
+                />
+                <Tooltip title="You are the owner of this account." arrow>
+                  <Typography
+                    variant="body1"
+                    component="p"
+                    sx={{
+                      fontSize: "0.8rem",
+                      color: theme.palette.primary.main,
+                    }}
+                  >
+                    Owner
+                  </Typography>
+                </Tooltip>
+              </Box>
+              <Box>
+                <Tooltip title="Threshold" arrow>
+                  <Typography
+                    variant="body1"
+                    component="p"
+                    sx={{ fontSize: "0.8rem", color: "#aaaaaa" }}
+                  >
+                    1/3
+                  </Typography>
+                </Tooltip>
+              </Box>
+              <Box>
+                <ArrowForwardIosRoundedIcon
+                  sx={{ fontSize: "1.2rem", color: "#4d4d4d" }}
+                />
+              </Box>
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </MainContentCard>
+  );
+}
--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,30 +1,22 @@
-import { HowToReg } from "@mui/icons-material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
-import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
-import {
-  Box,
-  Button,
-  CircularProgress,
-  Tooltip,
-  Typography,
-} from "@mui/material";
+import { Box, Button, CircularProgress, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { ReactNode, useEffect, useState } from "react";
-import { ChainId } from "useink/dist/chains";
 import { ArrayOneOrMore } from "useink/dist/core";
 
 import ErrorMessage from "@/components/common/ErrorMessage";
 import { BasicLayout } from "@/components/layout/BasicLayout";
 import { MainContentCard } from "@/components/layout/shared/MainContentCard";
-import NetworkBadge from "@/components/NetworkBadge";
-import { AccountSigner } from "@/components/StepperSignersAccount/AccountSigner";
-import { ChainExtended, getChain } from "@/config/chain";
+import {
+  MultisigsDataFormatted,
+  XsignersAccountTable,
+} from "@/components/XsignersAccountTable";
+import { getChain } from "@/config/chain";
 import { ROUTES } from "@/config/routes";
 import { useLocalDbContext } from "@/context/uselocalDbContext";
 import { usePolkadotContext } from "@/context/usePolkadotContext";
-import { Owner, SignatoriesAccount } from "@/domain/SignatoriesAccount";
+import { Owner } from "@/domain/SignatoriesAccount";
 import {
   useAddSignersAccount,
   useListSignersAccount,
@@ -33,33 +25,16 @@ import { useSetXsignerSelected } from "@/hooks/xsignerSelected/useSetXsignerSele
 import { generateRandomWalletName } from "@/utils/blockchain";
 import { customReportError } from "@/utils/error";
 
-type MultisigsDataFormatted = {
-  name: string;
-  address: string;
-  networkId?: ChainId;
-  network?: ChainExtended;
-};
 export default function WelcomePage() {
   const [loading, setLoading] = useState(false);
   const [multisigs, setMultisigs] = useState<MultisigsDataFormatted[]>([]);
   const [error, setError] = useState<string | null>(null);
   const { accountConnected, network } = usePolkadotContext();
-  const { logo, name: networkName } = getChain(network);
   const { data: signersAccount } = useListSignersAccount();
   const { save } = useAddSignersAccount();
   const { xsignerOwnersRepository } = useLocalDbContext();
   const { setXsigner } = useSetXsignerSelected();
-  const router = useRouter();
-  const handleMultisigRedirect = (address: string) => {
-    const selectedMultisig = multisigs?.find(
-      (multisig) => multisig.address === address
-    );
-    if (!selectedMultisig) {
-      return;
-    }
-    setXsigner(selectedMultisig as SignatoriesAccount);
-    router.replace(ROUTES.App);
-  };
+  const theme = useTheme();
 
   useEffect(() => {
     setError(null);
@@ -130,8 +105,6 @@ export default function WelcomePage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accountConnected?.address, network, save, signersAccount?.length]);
 
-  console.log(multisigs);
-  const theme = useTheme();
   return (
     <Box>
       {error && <ErrorMessage message={error} />}
@@ -187,105 +160,17 @@ export default function WelcomePage() {
           </Box>
         </Box>
       </MainContentCard>
-      <MainContentCard
-        stylesContainer={{
-          alignItems: "center",
-          sx: {
-            margin: { xs: 1, sm: 2, md: 2, lg: 3, xl: 3 },
-          },
-        }}
-      >
-        <Box display="flex" alignItems="center" gap={1.25}>
-          <Typography variant="h3" color="white">
-            My XSigners accounts ({multisigs.length})
-          </Typography>
-          <Typography variant="body1" component="p">
-            on
-          </Typography>
-          <NetworkBadge
-            showTooltip={false}
-            name={networkName}
-            logo={logo.src}
-            logoSize={{ width: 20, height: 20 }}
-            description={logo.alt}
-          />
+      {!loading ? (
+        <XsignersAccountTable
+          network={network}
+          onClick={setXsigner}
+          multisigs={multisigs}
+        />
+      ) : (
+        <Box display="flex" alignItems="center" justifyContent="center" p={2}>
+          <CircularProgress size={50} />
         </Box>
-        <Box
-          mt={3}
-          width="100%"
-          sx={{ backgroundColor: theme.palette.background.paper }}
-          p={0}
-          pt={0}
-          pb={0}
-        >
-          {!loading ? (
-            multisigs.map((multisig) => (
-              <Box key={multisig.address}>
-                <Box
-                  onClick={() => handleMultisigRedirect(multisig.address)}
-                  display="flex"
-                  gap={8}
-                  alignItems="center"
-                  justifyContent="space-around"
-                  sx={{
-                    borderBottom: "1px solid #2F2F2F",
-                    borderTop: "1px solid #2F2F2F",
-                    "&:hover": { backgroundColor: "#2F2F2F" },
-                  }}
-                  p={1}
-                  pl={0}
-                >
-                  <Box width={300}>
-                    <AccountSigner
-                      name={multisig.name}
-                      address={multisig.address}
-                      truncateAmount={16}
-                      showLink={false}
-                    />
-                  </Box>
-                  <Box display="flex" alignItems="center" gap={1}>
-                    <HowToReg sx={{ fontSize: "1.4rem", color: "#97b42d" }} />
-                    <Tooltip title="You are the owner of this account." arrow>
-                      <Typography
-                        variant="body1"
-                        component="p"
-                        sx={{ fontSize: "0.8rem", color: "#97b42d" }}
-                      >
-                        Owner
-                      </Typography>
-                    </Tooltip>
-                  </Box>
-                  <Box>
-                    <Tooltip title="Threshold" arrow>
-                      <Typography
-                        variant="body1"
-                        component="p"
-                        sx={{ fontSize: "0.8rem", color: "#aaaaaa" }}
-                      >
-                        1/3
-                      </Typography>
-                    </Tooltip>
-                  </Box>
-                  <Box>
-                    <ArrowForwardIosRoundedIcon
-                      sx={{ fontSize: "1.2rem", color: "#4d4d4d" }}
-                    />
-                  </Box>
-                </Box>
-              </Box>
-            ))
-          ) : (
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              p={2}
-            >
-              <CircularProgress size={50} />
-            </Box>
-          )}
-        </Box>
-      </MainContentCard>
+      )}
     </Box>
   );
 }
