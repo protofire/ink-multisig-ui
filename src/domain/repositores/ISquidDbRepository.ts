aaa517215a2ea67942657db2d67d17c39465bf21
0xLucca <0xlucca.dev@gmail.com>
2023-12-20 09:53:34 -0300

    Improve interface

--- src/domain/repositores/ISquidDbRepository.ts
+++ src/domain/repositores/ISquidDbRepository.ts
@@ -1,11 +1,16 @@
 export interface ExternalTransactionData {
   txHash: string;
   methodName: string;
-  args: [string];
+  args: ContractParam[];
   creationTimestamp: Date;
   inUse: boolean;
 }
 
+export interface ContractParam {
+  name: string;
+  value: string;
+}
+
 export interface ISquidDbRepository {
-  insertTxData(extTxData: ExternalTransactionData): Promise<number>;
+  insertTxData(extTxData: ExternalTransactionData): Promise<string>;
 }
