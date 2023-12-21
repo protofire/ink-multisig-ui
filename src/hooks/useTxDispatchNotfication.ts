a67b6ae17bf52c3f93329cea776e55255d6bfaed
Henry Palacios <henrydpalacios@gmail.com>
2023-12-21 10:47:52 -0300

    Hide status None

--- src/hooks/useTxDispatchNotfication.ts
+++ src/hooks/useTxDispatchNotfication.ts
@@ -19,6 +19,16 @@ export function useTxDispatchNotification({
       const id = toast.loading(`${msgPrefix} waiting for signature...`);
       setIdToast(id);
     }
+    if (idToast && tx.status === "None") {
+      // TODO, check this case
+      // toast.update(idToast, {
+      //   render: `The ${msgPrefix} was cancelled`,
+      //   type: "info",
+      //   isLoading: false,
+      //   autoClose: 500,
+      // });
+      setIdToast(0);
+    }
     if (idToast && tx.status === "Broadcast") {
       toast.update(idToast, {
         render: `The ${msgPrefix} will be proccessed in a block`,
