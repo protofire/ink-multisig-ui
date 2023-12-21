d01be3a470a1ad0046a6fe83df2bc5195bc9b14d
Henry Palacios <henrydpalacios@gmail.com>
2023-12-21 13:17:07 -0300

    Adding environmetn variables

--- src/config/squid.ts
+++ src/config/squid.ts
@@ -1,4 +1,13 @@
 import { config } from "@/services/squid/squidConfig";
 import { ChainId } from "@/services/useink/types";
+import { getMandatoryVariable } from "@/utils/getMandatoryVariable";
 
 export const squidConfig: Partial<Record<ChainId, string>> = config;
+
+export const NEXT_DB_USER = getMandatoryVariable("NEXT_DB_USER");
+export const NEXT_DB_PASS = getMandatoryVariable("NEXT_DB_PASS");
+export const NEXT_DB_HOST = getMandatoryVariable("NEXT_DB_HOST");
+export const NEXT_DB_PORT = process.env.NEXT_DB_PORT
+  ? parseInt(process.env.NEXT_DB_PORT)
+  : 23798;
+export const NEXT_DB_NAME = process.env.NEXT_DB_NAME || "squid";
