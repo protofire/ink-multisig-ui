c866d6bb1fff234438c255e13e4a9d0251d419cb
AndrN <andre.neyra@gmail.com>
2023-12-27 17:11:34 -0500

    add advanced details missing properties (#104)

--- src/components/TxTable/TxDetail/ReceivedDetail.tsx
+++ src/components/TxTable/TxDetail/ReceivedDetail.tsx
@@ -1,27 +1,20 @@
 import { Grid } from "@mui/material";
-import { ChainId } from "useink/dist/chains";
 
 import { ExtendedDataType } from "@/domain/repositores/ITxQueueRepository";
 import { formatDate } from "@/utils/formatString";
 
 import { CustomGridItem } from "./styled";
 
-export const ReceivedDetail = ({
-  data,
-  network,
-}: {
-  data: ExtendedDataType;
-  network: ChainId;
-}) => {
+export const ReceivedDetail = ({ data }: { data: ExtendedDataType }) => {
   const date = formatDate(data.creationTimestamp);
 
   return (
     <>
       <Grid container>
-        <CustomGridItem colType="name">Transaction hash</CustomGridItem>
-        <CustomGridItem colType="value">{data.id}</CustomGridItem>
         <CustomGridItem colType="name">Created:</CustomGridItem>
         <CustomGridItem colType="value">{date}</CustomGridItem>
+        <CustomGridItem colType="name">Transaction hash</CustomGridItem>
+        <CustomGridItem colType="value">{data.proposalTxHash}</CustomGridItem>
       </Grid>
     </>
   );
