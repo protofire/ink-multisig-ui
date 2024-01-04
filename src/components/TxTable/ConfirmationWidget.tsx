3a6b964457c6c1f561611f823eb6ce2a941abccd
0xLucca <0xlucca.dev@gmail.com>
2024-01-04 12:06:13 -0300

    Use hook useDryRunExecution

--- src/components/TxTable/ConfirmationWidget.tsx
+++ src/components/TxTable/ConfirmationWidget.tsx
@@ -1,14 +1,13 @@
 import { Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useMemo } from "react";
 
 import { LoadingButton } from "@/components/common/LoadingButton";
 import { usePolkadotContext } from "@/context/usePolkadotContext";
-import { useGetDryRun } from "@/hooks/useGetDryRun";
+import { useDryRunExecution } from "@/hooks/useDryRunExecution";
 import { TWO_SECONDS, useRecentlyClicked } from "@/hooks/useRecentlyClicked";
 import { ContractPromise } from "@/services/substrate/types";
 import { shouldDisable } from "@/services/useink/utils";
-import { customReportError } from "@/utils/error";
 
 import { DryRunMessage } from "../TxBuilderStepper/MethodSelectorStep/DryRunMessage";
 import { useContractTx } from "../TxBuilderStepper/ProposeTxStep/useContractTx";
@@ -20,50 +19,27 @@ export function ConfirmationWidget({
   multisigContractPromise: ContractPromise;
   txId: string;
 }) {
-  const [outcome, setOutcome] = useState<string>();
-  const [error, setError] = useState<string>();
-  const [isRunning, setIsRunning] = useState(true);
+  const approveMessage = useMemo(
+    () => multisigContractPromise.abi.findMessage("approveTx"),
+    [multisigContractPromise.abi]
+  );
   const { accountConnected } = usePolkadotContext();
+  const { outcome, error, isRunning, executeDryRun } = useDryRunExecution({
+    contractPromise: multisigContractPromise,
+    message: approveMessage,
+    params: [parseInt(txId)],
+    substrateRegistry: multisigContractPromise.registry,
+    addressCaller: accountConnected?.address,
+    autoRun: true,
+    successOutcome: "You can vote",
+    failureOutcome: "You can't vote",
+  });
+
   const { recentlyClicked: recentlyClickedApprove, ref: refButtonApprove } =
     useRecentlyClicked(TWO_SECONDS);
   const { recentlyClicked: recentlyClickedReject, ref: refButtonReject } =
     useRecentlyClicked(TWO_SECONDS);
   const theme = useTheme();
-
-  const reset = () => {
-    setOutcome(undefined);
-    setError(undefined);
-    setIsRunning(true);
-  };
-
-  const approveDryRun = useGetDryRun(
-    multisigContractPromise,
-    "approveTx",
-    accountConnected?.address
-  );
-
-  const executeApproveDryRun = useCallback(async () => {
-    setIsRunning(true);
-    try {
-      const dryRunResult = await approveDryRun.send([parseInt(txId)]);
-      if (!dryRunResult?.ok) {
-        throw new Error(
-          dryRunResult?.error.toString() ??
-            "Error on executing the transaction."
-        );
-      } else if (dryRunResult.value.decoded.Err) {
-        setOutcome("You can't vote: " + dryRunResult.value.decoded.Err);
-        setError(dryRunResult.value.decoded.Err);
-      } else {
-        setOutcome("You can vote");
-      }
-    } catch (e) {
-      const errorFormatted = customReportError(e);
-      setError(errorFormatted);
-    } finally {
-      setIsRunning(false);
-    }
-  }, [approveDryRun, txId]);
 
   const approveTx = useContractTx({
     contractPromise: multisigContractPromise,
@@ -82,8 +58,7 @@ export function ConfirmationWidget({
   });
 
   useEffect(() => {
-    reset();
-    executeApproveDryRun();
+    executeDryRun();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accountConnected?.address, approveTx.outcome, rejectTx.outcome]);
 
@@ -100,7 +75,11 @@ export function ConfirmationWidget({
         mt={0}
         flexDirection="column" // Change to column layout
       >
-        <DryRunMessage outcome={outcome} error={error} isRunning={isRunning} />
+        <DryRunMessage
+          outcome={error ? `${outcome}: ${error}` : outcome}
+          error={error}
+          isRunning={isRunning}
+        />
         <Box
           display="flex"
           alignItems="center"
