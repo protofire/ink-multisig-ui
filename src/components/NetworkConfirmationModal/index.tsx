d40a6808e39690bc887898f15f7af00a3164094a
Agustín Longoni <agustin.longoni@altoros.com>
2024-01-04 13:59:12 -0300

    draft refactor of network component

--- src/components/NetworkConfirmationModal/index.tsx
+++ src/components/NetworkConfirmationModal/index.tsx
@@ -1,6 +1,3 @@
-import { Button, Modal, Typography } from "@mui/material";
-import Box from "@mui/material/Box";
-import { useTheme } from "@mui/material/styles";
 import { useRouter } from "next/router";
 import * as React from "react";
 import { useEffect, useState } from "react";
@@ -8,10 +5,16 @@ import { useEffect, useState } from "react";
 import { getChain } from "@/config/chain";
 import { ROUTES } from "@/config/routes";
 import { usePolkadotContext } from "@/context/usePolkadotContext";
+import { useModalBehaviour } from "@/hooks/useModalBehaviour";
 import { useGetXsignerSelected } from "@/hooks/xsignerSelected/useGetXsignerSelected";
 
+import BaseConfirmationDialog from "../BaseConfirmationDialog";
+
+const message =
+  "This ink account exists on {networkName} network. Do you want to \
+  change the network to be able to see the account?";
+
 export default function NetworkConfirmationModal() {
-  const theme = useTheme();
   const { network, setNetwork } = usePolkadotContext();
   const { xSignerSelected } = useGetXsignerSelected();
   const router = useRouter();
@@ -33,54 +36,15 @@ export default function NetworkConfirmationModal() {
       setOpen(true);
     }
   }, [network, xSignerSelected]);
+  const { closeModal, isOpen, openModal } = useModalBehaviour();
 
   return (
-    <Box sx={{ width: "100%" }}>
-      <Modal
-        open={open}
-        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
-      >
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          flexDirection="column"
-          p={2}
-          m={2}
-          width={500}
-          sx={{ backgroundColor: theme.palette.grey.A100 }}
-        >
-          <Typography variant="h4" color="primary" m={2}>
-            Change Network confirmation
-          </Typography>
-          <Typography variant="h6" m={2}>
-            This ink account exists on {networkName} network. Do you want to
-            change the network to be able to see the account?
-          </Typography>
-          <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
-            width={300}
-            mt={3}
-          >
-            <Button
-              onClick={handleConfirm}
-              variant="contained"
-              sx={{ width: 134 }}
-            >
-              Confirm
-            </Button>
-            <Button
-              onClick={handleCancel}
-              variant="outlined"
-              sx={{ width: 94 }}
-            >
-              Cancel
-            </Button>
-          </Box>
-        </Box>
-      </Modal>
-    </Box>
+    <BaseConfirmationDialog
+      title="Change Network confirmation"
+      message={message}
+      open={isOpen}
+      onClose={closeModal}
+      onConfirm={() => console.log}
+    />
   );
 }
