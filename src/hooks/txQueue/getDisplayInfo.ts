6aa986753be27444f4ac957a21028e162e33a71f
0xLucca <0xlucca.dev@gmail.com>
2024-01-04 13:08:34 -0300

    wip: Decoding working

--- src/hooks/txQueue/getDisplayInfo.ts
+++ src/hooks/txQueue/getDisplayInfo.ts
@@ -7,6 +7,7 @@ import { MultisigSdk, Psp22Sdk } from "xsigners-sdk-test";
 
 import { ChainExtended } from "@/config/chain";
 import { TX_TYPE_IMG } from "@/config/images";
+import { ContractParam } from "@/domain/repositores/ISquidDbRepository";
 import { TransactionProposed } from "@/domain/TransactionProposed";
 import { TransactionDisplayInfo } from "@/domain/TransactionProposedItemUi";
 import { ApiPromise, ContractPromise } from "@/services/substrate/types";
@@ -52,21 +53,58 @@ export const getDisplayInfo = async ({
     multisigAddress
   );
 
-  if (
-    multisigAddress === txProposed.contractAddress &&
-    txProposed.selector === TRANSFER_METHOD_SELECTOR
-  ) {
-    const decodedData = decodeCallArgs(
-      contractPromise,
-      "transfer",
-      txProposed.rawArgs as string // always will be string in native transfer
+  if (multisigAddress === txProposed.contractAddress) {
+    const message = contractPromise.abi.messages.find(
+      (message) => message.selector.toString() === txProposed.selector
     );
 
-    displayInfo["valueAmount"] = `${parseNativeBalance(decodedData[1])} ${
-      nativeToken.token
-    }`;
-    displayInfo["to"] = decodedData[0];
-    displayInfo["img"] = TX_TYPE_IMG.SEND;
+    if (message === undefined) {
+      displayInfo["to"] = txProposed.contractAddress;
+      displayInfo["valueAmount"] = `${balanceToFixed(
+        txProposed.value,
+        nativeToken.decimals
+      )} ${nativeToken.token}`;
+      displayInfo["type"] = txProposed.methodName || txProposed.selector;
+      return displayInfo;
+    }
+    const methodName = message?.method;
+    const decodedData = decodeCallArgs(
+      contractPromise,
+      methodName!,
+      txProposed.rawArgs as string // Always will be string
+    );
+
+    switch (methodName) {
+      case "transfer": {
+        displayInfo["valueAmount"] = `${parseNativeBalance(decodedData[1])} ${
+          nativeToken.token
+        }`;
+        displayInfo["to"] = decodedData[0];
+        displayInfo["img"] = TX_TYPE_IMG.SEND;
+        break;
+      }
+      default: {
+        // Update txProposed with decoded data
+        txProposed.methodName = methodName!;
+        txProposed.args;
+        const args: ContractParam[] = [];
+        decodedData.forEach((arg: any, index: number) => {
+          args.push({
+            name: message?.args[index].name,
+            value: arg,
+          });
+        });
+        txProposed.args = args;
+        displayInfo["to"] = txProposed.contractAddress;
+        displayInfo["valueAmount"] = `${balanceToFixed(
+          txProposed.value,
+          nativeToken.decimals
+        )} ${nativeToken.token}`;
+        displayInfo["type"] = methodName;
+        //TODO: Maybe add setting img
+        break;
+      }
+    }
   } else if (txProposed.selector === PSP22_TRANSFER_METHOD_SELECTOR) {
     contractPromise = new ContractPromise(
       apiPromise,
