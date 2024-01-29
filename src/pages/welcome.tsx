f91504825f9e15cd9740b13b560f60a42c20d944
Agustín Longoni <agustin.longoni@protofire.io>
2024-01-29 09:51:56 -0300

    add footer on welcome (#142)

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -6,6 +6,7 @@ import { ReactNode, useEffect } from "react";
 
 import ErrorMessage from "@/components/common/ErrorMessage";
 import { BasicLayout } from "@/components/layout/BasicLayout";
+import { Footer } from "@/components/layout/Footer";
 import { MainContentCard } from "@/components/layout/shared/MainContentCard";
 import { XsignersAccountTable } from "@/components/XsignersAccountTable";
 import { ROUTES } from "@/config/routes";
@@ -93,6 +94,9 @@ export default function WelcomePage() {
           <CircularProgress size={50} />
         </Box>
       )}
+      <Box sx={{ padding: "0 15.5rem 1rem 0" }}>
+        <Footer />
+      </Box>
     </Box>
   );
 }
